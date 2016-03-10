133500cb32614243b416c378542b312ec06097a7
sunzequn <sunzequn@live.cn>
2016-03-10 11:13:13 +0800

    change os to Ubuntu

--- src/main/java/cn/edu/nju/ws/geokb/controller/IndexController.java
+++ src/main/java/cn/edu/nju/ws/geokb/controller/IndexController.java
@@ -1,7 +1,5 @@
 package cn.edu.nju.ws.geokb.controller;
 
-import cn.edu.nju.ws.geokb.dao.ChinaCityDao;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,7 +13,6 @@ public class IndexController {
 
     @RequestMapping(value = "/index", method = RequestMethod.GET)
     public String index() {
-        System.out.println("test");
         return "index";
     }
 }
